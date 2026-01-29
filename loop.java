5836f8b07947a7c7f9b2c216c2c6ef5b9beccf4f
Rushikesh Sunil Mangalkar <150776220+CodeX-RVSM@users.noreply.github.com>
2026-01-29 17:30:14 +0530

    Create loop.java

---
+++ loop.java
@@ -0,0 +1,11 @@
+class loop {
+    public static void main(String[] args){
+        int age=25;
+        if(age>=18){
+            System.out.println("Yes!!!!...eligible");
+        }
+        else{
+            System.out.println("Not elegible");
+        }
+    }
+}
