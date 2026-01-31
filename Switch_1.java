889b0825d34062009340f68a1b4ea6adc15578f2
Rushikesh Sunil Mangalkar <150776220+CodeX-RVSM@users.noreply.github.com>
2026-01-31 23:30:04 +0530

    Create Switch_1.java

---
+++ Switch_1.java
@@ -0,0 +1,15 @@
+class Switch_1{
+    public static void main(String[] args){
+        int month=4;
+        String result=switch(month){
+        case 1,3,5,7,8,12->"31 days";
+        case 4,9->"30 days";
+        case 2->"28/29 days";
+        default ->"invalid month number";
+    };
+    System.out.println(result);
+
+
+    }
+    
+}
