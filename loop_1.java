7370fc12e110a273eabe05fc2f5b91e944b2dbd3
Rushikesh Sunil Mangalkar <150776220+CodeX-RVSM@users.noreply.github.com>
2026-01-31 23:27:32 +0530

    Create loop_1.java

---
+++ loop_1.java
@@ -0,0 +1,28 @@
+class loop_1{
+    
+    static int a;
+    static String s;
+    static char c;
+    
+    static double d;
+    static short e;
+    static float f;
+    static byte g;
+    static long h;
+    static boolean j;
+
+
+
+    public static void main(String[] args) {
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+        System.out.println(j);
+
+    }
+}
