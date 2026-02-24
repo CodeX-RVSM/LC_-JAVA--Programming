34b7a6de634696ab095e654d089e9d17954c67a9
Rushikesh Sunil Mangalkar <150776220+CodeX-RVSM@users.noreply.github.com>
2026-02-24 22:54:32 +0530

    Create vehical.java

---
+++ inheritance/vehical.java
@@ -0,0 +1,13 @@
+package com.linkcode.inheritance;
+
+public class vehical {
+
+
+	    String brand;
+	    int speed;
+
+	    public void start() {
+	        System.out.println("Vehicle is starting...");
+	    }
+	
+}
